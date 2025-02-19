cbb929d904563766105586e40843297f588392bb
Aziz-khan88 <aziz.rehman@crystallitedigital.com>
2025-02-19 12:48:15 +0500

    tag updated

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -39,27 +39,97 @@ export default function Home() {
       <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{
-          __html: `
-              {
-                "@context": "https://schema.org/", 
-                "@type": "Product", 
-                "name": "Infiniti Digital",
-                "image": "https://www.infinitidigital.us/infinitidigital.png",
-                "url": "https://www.infinitidigital.us/",
-                "description": "Attract, Impress, and make more revenue with full-service digital marketing agency. We provide SEO, PPC, Social Media, Web Development and more!",
-                "brand": {
-                  "@type": "Brand",
-                  "name": "Infiniti Digital"
-                },
-                "aggregateRating": {
-                  "@type": "AggregateRating",
-                  "ratingValue": "4.5",
-                  "ratingCount": "702"
-                }
+          __html: JSON.stringify([
+            {
+              "@context": "https://schema.org/",
+              "@type": "Product",
+              "name": "Infiniti Digital",
+              "image": "https://www.infinitidigital.us/infinitidigital.png",
+              "url": "https://www.infinitidigital.us/",
+              "description": "Attract, Impress, and make more revenue with full-service digital marketing agency. We provide SEO, PPC, Social Media, Web Development and more!",
+              "brand": {
+                "@type": "Brand",
+                "name": "Infiniti Digital"
+              },
+              "aggregateRating": {
+                "@type": "AggregateRating",
+                "ratingValue": "4.5",
+                "ratingCount": "702"
               }
-            `,
+            },
+            {
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              "name": "Infiniti Digital",
+              "alternateName": "InfinitiDigital",
+              "url": "https://www.infinitidigital.us/",
+              "logo": "https://www.infinitidigital.us/infinitidigital.png",
+              "contactPoint": {
+                "@type": "ContactPoint",
+                "telephone": "+1-655-487-632",
+                "contactType": "sales",
+                "contactOption": "TollFree",
+                "areaServed": "US",
+                "availableLanguage": "en"
+              },
+              "sameAs": [
+                "https://www.facebook.com/infinitidigitalus/",
+                "https://www.instagram.com/infinitidigitalus/",
+                "https://x.com/Infinitidigitl",
+                "https://www.pinterest.com/infinitidigitalus/",
+                "https://www.youtube.com/@InfinitiDigitalus",
+                "https://www.linkedin.com/company/infiniti-digitalus"
+              ]
+            },
+            {
+              "@context": "https://schema.org",
+              "@type": "ProfessionalService",
+              "name": "Infiniti Digital",
+              "image": "https://www.infinitidigital.us/infinitidigital.png",
+              "@id": "",
+              "url": "https://www.infinitidigital.us/",
+              "telephone": "+1-655-487-632",
+              "priceRange": "399 to 699",
+              "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "9402 Synott Rd Apt 114",
+                "addressLocality": "Houston",
+                "addressRegion": "TX",
+                "postalCode": "77083",
+                "addressCountry": "US"
+              },
+              "geo": {
+                "@type": "GeoCoordinates",
+                "latitude": 29.677372631618237,
+                "longitude": -95.6151225190571
+              },
+              "openingHoursSpecification": {
+                "@type": "OpeningHoursSpecification",
+                "dayOfWeek": [
+                  "Monday",
+                  "Tuesday",
+                  "Wednesday",
+                  "Thursday",
+                  "Friday",
+                  "Saturday",
+                  "Sunday"
+                ],
+                "opens": "00:00",
+                "closes": "23:59"
+              },
+              "sameAs": [
+                "https://www.facebook.com/infinitidigitalus/",
+                "https://www.instagram.com/infinitidigitalus/",
+                "https://x.com/Infinitidigitl",
+                "https://www.pinterest.com/infinitidigitalus/",
+                "https://www.youtube.com/@InfinitiDigitalus",
+                "https://www.linkedin.com/company/infiniti-digitalus"
+              ]
+            }
+          ])
         }}
       />
+
     </>
   );
 }
