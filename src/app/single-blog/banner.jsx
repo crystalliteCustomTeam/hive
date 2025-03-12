f4c598cb6c7cd1df845ca95b230fbb6ccce42d53
Aziz Ur Rehman <aziz.rehman@dreamweaveforce.com>
2025-03-12 12:30:51 +0500

    content upadted

--- src/app/single-blog/banner.jsx
+++ src/app/single-blog/banner.jsx
@@ -9,13 +9,7 @@ const Banner = ({ data }) => {
     <>
       <section className={styles.singleBlogs}>
         <Container>
-          <Row>
-            <Col md={12} className="m-auto text-center">
-              <div className={styles.singleBlogImg}>
-                <Image src={data.img} alt="Single Blog" fill />
-              </div>
-            </Col>
-          </Row>
+     
           <Row>
             <Col md={12} lg={12} className="m-auto">
               <div className={styles.singleBlogTxt}>
