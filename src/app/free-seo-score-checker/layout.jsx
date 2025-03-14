95017670f7250817dc78c6a331f5bb1a5a6fb450
Aziz Ur Rehman <aziz.rehman@dreamweaveforce.com>
2025-03-14 16:06:21 +0500

    metas updated

--- src/app/free-seo-score-checker/layout.jsx
+++ src/app/free-seo-score-checker/layout.jsx
@@ -2,25 +2,21 @@
 //===== Meta Data =====
 export const metadata = {
   //===== Meta Tags =====
-  title: "Insightful Blogs on Digital Marketing, & Mobile Apps",
-  description: "Read our blogs as we provide insights on digital marketing strategies, wordpress deveopment tips, SEO, PPC, mobile apps & more!",
+  title: "Free SEO Score Checker - Infiniti Digital",
+  description: "Get your website's SEO score and see how much effort you need to rank higher on SERP. Click on our free SEO score checker and fill the SEO scorecard.",
   //===== OG Tags =====
   openGraph: {
-    title: "Insightful Blogs on Digital Marketing, & Mobile Apps",
-    description: "Read our blogs as we provide insights on digital marketing strategies, wordpress deveopment tips, SEO, PPC, mobile apps & more!",
-    url: '/single-blog',
+    title: "Free SEO Score Checker - Infiniti Digital",
+    description: "Get your website's SEO score and see how much effort you need to rank higher on SERP. Click on our free SEO score checker and fill the SEO scorecard.",
+    url: '/free-seo-score-checker',
     siteName: "Inifniti Digital",
     locale: 'en_US',
     type: 'website',
     images: "https://www.infinitidigital.us/infinitidigital.png",
   },
   //===== Canonical =====
-  alternates: { canonical: '/single-blog' },
-    //===== No-Index =====
-    robots: {
-      index: false,
-      follow: false,
-    },
+  alternates: { canonical: '/free-seo-score-checker' },
+   
 }
 
 
