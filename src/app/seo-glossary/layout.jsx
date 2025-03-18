988d6facd44311864b5a82f42ee2b7d0cde6992a
Aziz Ur Rehman <aziz.rehman@dreamweaveforce.com>
2025-03-18 14:57:29 +0500

    contetn upadted glossry

--- src/app/seo-glossary/layout.jsx
+++ src/app/seo-glossary/layout.jsx
@@ -3,11 +3,11 @@
 export const metadata = {
     //===== Meta Tags =====
     title: "SEO Glossary: 400+  Terms & Definitions You Need to Know",
-    description: "Check out this HUGE list of the most important SEO terms, jargon, abbreviations to help you better understand digital marketing.",
+    description: "Check out this HUGE list of the most important SEO terms, jargon, abbreviations to help you better understand SEO.",
     //===== OG Tags =====
     openGraph: {
         title: "SEO Glossary: 400+  Terms & Definitions You Need to Know",
-        description: "Check out this HUGE list of the most important SEO terms, jargon, abbreviations to help you better understand digital marketing.",
+        description: "Check out this HUGE list of the most important SEO terms, jargon, abbreviations to help you better understand SEO.",
         url: '/seo-glossary',
         siteName: "Inifniti Digital",
         locale: 'en_US',
