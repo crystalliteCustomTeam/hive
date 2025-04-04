3712921fb1d38292521ab569806f7d0f0a8ca2ed
Aziz Ur Rehman <aziz.rehman@dreamweaveforce.com>
2025-04-04 17:17:44 +0500

    optimzation start from Home apge

--- src/app/home/banner.jsx
+++ src/app/home/banner.jsx
@@ -6,7 +6,6 @@ import CommonBtn from "@/src/app/components/common/commonbtn"
 
 const Banner = ({ data }) => {
 
-
     return (
         <section className={styles.MainBanner}>
             <Container className="h-100">
@@ -29,7 +28,17 @@ const Banner = ({ data }) => {
                 className={styles.bannerVideo}
                 loading="eager"
                 poster={data.BannerPoster}
-            >
+                playsInline
+                webkit-playsinline="true" 
+                x-webkit-airplay="allow"
+                x-webkit-media-controls="false" 
+                x-webkit-playsinline="true"
+                x-webkit-user-drag="none"
+                x-webkit-user-select="none"
+                x-webkit-text-size-adjust="none"
+                x-webkit-tap-highlight-color="rgba(0, 0, 0, 0)"
+                x-webkit-overflow-scrolling="touch"
+                x-webkit-speech="true">
                 <source src={data.video} type="video/mp4" />
             </video>
             <Clients data={ClientImages} />
