a84d5b124d7244b6f9df1d8f70a1c094bc36bc9b
Aziz Ur Rehman <aziz.rehman@dreamweaveforce.com>
2025-04-24 13:43:15 +0500

    site map

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-    siteUrl: 'https://www.infinitidigital.us', 
-    generateRobotsTxt: true,
-    sitemapSize: 5000,
-    changefreq: 'weekly',
-    priority: 0.7,
+  siteUrl: 'https://www.infinitidigital.us',
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 5000, // optional
+  exclude: [], // optional, make sure you’re not excluding that page!
   };
