bdad7aba930c753a73f8a61b96fe56ed21f2fc2e
Aziz Ur Rehman <aziz.rehman@dreamweaveforce.com>
2025-05-02 19:13:11 +0500

    Hive home page sehema added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -67,93 +67,160 @@ export default function Home() {
             {
               "@context": "https://schema.org",
               "@type": "ProfessionalService",
-              name: "Infiniti Digital",
-              image: "https://www.infinitidigital.us/infinitidigital.png",
+              "name": "Infiniti Digital",
+              "image": "https://www.infinitidigital.us/infinitidigital.png",
               "@id": "",
-              url: "https://www.infinitidigital.us/",
-              telephone: "855-666-6682",
-              priceRange: "399 to 699",
-              address: {
+              "url": "https://www.infinitidigital.us/",
+              "telephone": "855-666-6682",
+              "priceRange": "399 to 699",
+              "address": {
                 "@type": "PostalAddress",
-                streetAddress: "9518 Mykawa Road",
-                addressLocality: "Houston",
-                addressRegion: "TX",
-                postalCode: "77048",
-                addressCountry: "US",
+                "streetAddress": "9518 Mykawa Road",
+                "addressLocality": "Houston",
+                "addressRegion": "TX",
+                "postalCode": "77048",
+                "addressCountry": "US"
               },
-              geo: {
+              "geo": {
                 "@type": "GeoCoordinates",
-                latitude: 29.642761,
-                longitude: -95.311412,
+                "latitude": 29.642761,
+                "longitude": -95.311412
               },
-              openingHoursSpecification: {
+              "openingHoursSpecification": {
                 "@type": "OpeningHoursSpecification",
-                dayOfWeek: [
+                "dayOfWeek": [
                   "Monday",
                   "Tuesday",
                   "Wednesday",
                   "Thursday",
                   "Friday",
                   "Saturday",
-                  "Sunday",
+                  "Sunday"
                 ],
-                opens: "00:00",
-                closes: "23:59",
+                "opens": "00:00",
+                "closes": "23:59"
               },
-              sameAs: [
+              "sameAs": [
                 "https://www.facebook.com/infinitidigitalus/",
                 "https://www.instagram.com/infinitidigitalus/",
                 "https://x.com/Infinitidigitl",
                 "https://www.pinterest.com/infinitidigitalus/",
                 "https://www.youtube.com/@InfinitiDigitalus",
-                "https://www.linkedin.com/company/infiniti-digitalus",
-              ],
+                "https://www.linkedin.com/company/infiniti-digitalus"
+              ]
             },
             {
               "@context": "https://schema.org/",
               "@type": "Product",
-              name: "Infiniti Digital",
-              image: "https://www.infinitidigital.us/infinitidigital.png",
-              url: "https://www.infinitidigital.us/",
-              description:
-                "Attract, Impress, and make more revenue with full-service digital marketing agency. We provide SEO, PPC, Social Media, Web Development and more!",
-              brand: {
+              "name": "Infiniti Digital",
+              "image": "https://www.infinitidigital.us/infinitidigital.png",
+              "url": "https://www.infinitidigital.us/",
+              "description": "Attract, Impress, and make more revenue with full-service digital marketing agency. We provide SEO, PPC, Social Media, Web Development and more!",
+              "brand": {
                 "@type": "Brand",
-                name: "Infiniti Digital",
+                "name": "Infiniti Digital"
               },
-              aggregateRating: {
+              "aggregateRating": {
                 "@type": "AggregateRating",
-                ratingValue: "4.9",
-                ratingCount: "1826",
-              },
+                "ratingValue": "4.9",
+                "ratingCount": "1826"
+              }
             },
             {
               "@context": "https://schema.org",
               "@type": "Organization",
-              name: "Infiniti Digital",
-              alternateName: "InfinitiDigital",
-              url: "https://www.infinitidigital.us/",
-              logo: "https://www.infinitidigital.us/infinitidigital.png",
-              contactPoint: {
+              "name": "Infiniti Digital",
+              "alternateName": "InfinitiDigital",
+              "url": "https://www.infinitidigital.us/",
+              "logo": "https://www.infinitidigital.us/infinitidigital.png",
+              "contactPoint": {
                 "@type": "ContactPoint",
-                telephone: "855-666-6682",
-                contactType: "sales",
-                contactOption: "TollFree",
-                areaServed: "US",
-                availableLanguage: "en",
+                "telephone": "855-666-6682",
+                "contactType": "sales",
+                "contactOption": "TollFree",
+                "areaServed": "US",
+                "availableLanguage": "en"
               },
-              sameAs: [
+              "sameAs": [
                 "https://www.facebook.com/infinitidigitalus/",
                 "https://www.instagram.com/infinitidigitalus/",
                 "https://x.com/Infinitidigitl",
                 "https://www.pinterest.com/infinitidigitalus/",
                 "https://www.youtube.com/@InfinitiDigitalus",
-                "https://www.linkedin.com/company/infiniti-digitalus",
-              ],
+                "https://www.linkedin.com/company/infiniti-digitalus"
+              ]
             },
-          ]),
+            // 🔽 SiteNavigationElement items
+            {
+              "@context": "https://schema.org",
+              "@type": "SiteNavigationElement",
+              "name": "Homepage",
+              "url": "https://www.infinitidigital.us/"
+            },
+            {
+              "@context": "https://schema.org",
+              "@type": "SiteNavigationElement",
+              "name": "Search Engine Optimization Services",
+              "url": "https://www.infinitidigital.us/search-engine-optimization-services"
+            },
+            {
+              "@context": "https://schema.org",
+              "@type": "SiteNavigationElement",
+              "name": "Best SEO Agency",
+              "url": "https://www.infinitidigital.us/best-seo-agency"
+            },
+            {
+              "@context": "https://schema.org",
+              "@type": "SiteNavigationElement",
+              "name": "Organic SEO Services",
+              "url": "https://www.infinitidigital.us/organic-seo-services"
+            },
+            {
+              "@context": "https://schema.org",
+              "@type": "SiteNavigationElement",
+              "name": "Local SEO Agency",
+              "url": "https://www.infinitidigital.us/local-seo-agency"
+            },
+            {
+              "@context": "https://schema.org",
+              "@type": "SiteNavigationElement",
+              "name": "PPC Management Services",
+              "url": "https://www.infinitidigital.us/ppc-management-services"
+            },
+            {
+              "@context": "https://schema.org",
+              "@type": "SiteNavigationElement",
+              "name": "Social Media Marketing",
+              "url": "https://www.infinitidigital.us/social-media-marketing"
+            },
+            {
+              "@context": "https://schema.org",
+              "@type": "SiteNavigationElement",
+              "name": "SEO Content Writing Services",
+              "url": "https://www.infinitidigital.us/seo-content-writing-services"
+            },
+            {
+              "@context": "https://schema.org",
+              "@type": "SiteNavigationElement",
+              "name": "Social Media Marketing Agency",
+              "url": "https://www.infinitidigital.us/social-media-marketing-agency"
+            },
+            {
+              "@context": "https://schema.org",
+              "@type": "SiteNavigationElement",
+              "name": "GMB Management Service",
+              "url": "https://www.infinitidigital.us/gmb-management-service"
+            },
+            {
+              "@context": "https://schema.org",
+              "@type": "SiteNavigationElement",
+              "name": "Social Media Advertising Agency",
+              "url": "https://www.infinitidigital.us/social-media-advertising-agency"
+            }
+          ])
         }}
       />
+
     </>
   );
 }
